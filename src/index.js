07ac08b94adb1e22214788c78e2ba5fea64da2b0
Levi Trejo <levi.b.trejo@gmail.com>
2021-01-08 08:12:42 -0600

    feat: lifting the state up

--- src/index.js
+++ src/index.js
@@ -4,30 +4,50 @@ import './index.css';
 
 class Square extends React.Component {
     // Add the constructor to initialize the state property
-    constructor(props) {
-        // Always call super when defining the constructor of a subclass
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+
+    // Square constructor is no longer needed once the board takes over management of the square state
+
+    // constructor(props) {
+    //     // Always call super when defining the constructor of a subclass
+    //     super(props);
+    //     this.state = {
+    //         value: null,  // The state is empty to start the game, will be updated to "X" or "O" in time
+    //     };
+    // }
 
 
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({value : 'X'})}
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return ( <Square
+                    value={this.state.squares[i]}
+                    onClick = {() => this.handleClick(i)}
+        />
+        );
     }
 
     render() {
